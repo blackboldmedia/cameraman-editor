41ba548947b8963628154a2c6a0104f29015dabe
tqtoan <tqtoan@gmail.com>
2022-03-22 10:14:30 +0700

    Update file

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,14 +33,14 @@ let swiper = new Swiper(".projects__container", {
     cssMode: true,
     loop: true,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    });
+});
 
 /*============ MENU HIDDEN ==========*/
 /*============ MENU HIDDEN ==========*/
